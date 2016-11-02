68a96daa70e9d6cde36a4d69bbd5db0499293be2
rohitkaushik03 <rohitkaushik03@gmail.com>
2016-11-01 17:21:03 -0700

    Added check Unique string function
    Added check Unique string function

--- src/HashTablesAlgos/HashAlgos.java
+++ src/HashTablesAlgos/HashAlgos.java
@@ -14,6 +14,7 @@ public class HashAlgos
         System.out.println(result);
     }
 
+
     public static boolean checkStringUniqueCharacter(String strInput)
     {
         //Implement an algorithm to determine if a string has all unique characters
