a40a8f361b97c2abe3ed8b95edb2feb36a4cafb9
rohitkaushik03 <rohitkaushik03@gmail.com>
2016-11-12 22:51:23 -0800

    Binary Search Tree Insert function
    Binary Search Tree Insert function

---
+++ src/BinaryTree/BSTMain.java
@@ -0,0 +1,16 @@
+package BinaryTree;
+
+/**
+ * Created by rkaushik on 11/11/16.
+ */
+public class BSTMain
+{
+    public static void main(String args[])
+    {
+        MyBST objBST = new MyBST();
+        objBST.InsertNode(10);
+        objBST.InsertNode(15);
+        objBST.InsertNode(9);
+        objBST.InsertNode(6);
+    }
+}
--- src/BinaryTree/MyBST.java
+++ src/BinaryTree/MyBST.java
@@ -7,13 +7,33 @@ public class MyBST
 {
     BSTNode root=null;
 
-    public void InsertNode(int data)
-    {
-        if (root == null)
+    public void InsertNode(int data) {
+        BSTNode newNode = new BSTNode(data);
+
+        if (root == null) {
+            root = newNode;
+            return;
+        }
+        BSTNode current = root;
+        BSTNode parent;
+
+        while (true)
         {
-            root = new BSTNode(data);
-            root.right = null;
-            root.left = null;
+            parent = current;
+
+            if (data < current.data) {
+                current = current.left;
+                if (current == null) {
+                    parent.left = newNode;
+                    return;
+                }
+            } else if (data > current.data) {
+                current = current.right;
+                if (current == null) {
+                    parent.right = newNode;
+                    return;
+                }
+            }
         }
     }
 }
