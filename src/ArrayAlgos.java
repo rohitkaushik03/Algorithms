7341ca153494dfb02aa75fd4cf5ac2a52ceadf59
Rohit <rohitkaushik03@gmail.com>
2018-05-03 22:18:34 -0700

    replace space with 20

--- src/ArrayAlgos.java
+++ src/ArrayAlgos.java
@@ -1069,5 +1069,35 @@ public class ArrayAlgos {
 
         return true;
     }
+
+
+/*    Write a method to replace all the spaces in a string with ‘%20’.
+    You may assume that the string has sufficient space at the end to hold the additional characters,
+    and that you are given the “true” length of the string.*/
+    public static void replaceSpace(char[] str, int length)
+    {
+        int newLength=0,spaceCount=0;
+
+        for (int i=0;i<length;i++) {
+            if (str[i]==' ') {
+                spaceCount++;
+            }
+        }
+        newLength = length + spaceCount*2;
+
+        //now start from backward
+        for(int i = length-1;i>0;i--) {
+            if (str[i]==' ') {
+                str[newLength -1]='0';
+                str[newLength -2]='2';
+                str[newLength -3]='%';
+                newLength = newLength -3;
+            }
+            else{
+                str[newLength] = str[i];
+                newLength--;
+            }
+        }
+    }
 }
 
