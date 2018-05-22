83d1a08e7b2abc7684de41fd765f4663443b7a75
Rohit <rohitkaushik03@gmail.com>
2018-05-21 22:28:50 -0700

    shortestDistance
    shortestDistance

--- src/ArrayAlgos.java
+++ src/ArrayAlgos.java
@@ -109,8 +109,10 @@ public class ArrayAlgos {
         String calInput="12+13";
         //int calResult =  CalculateFromString(calInput);
 
-        boolean resultP =  checkPermutation("test","estt");
+        //boolean resultP =  checkPermutation("test","estt");
 
+        String[] words = new String[]{"practice", "makes", "perfect", "coding", "makes"};
+        int res = shortestDistance(words,"coding","practice");
 
     }
 
@@ -1098,6 +1100,44 @@ public class ArrayAlgos {
                 newLength--;
             }
         }
+    }
+
+
+    /*Given a list of words and two words word1 and word2, return the shortest distance between these two words in the list.
+    For example,
+    Assume that words = ["practice", "makes", "perfect", "coding", "makes"].
+    Given word1 = “coding”, word2 = “practice”, return 3.
+    Given word1 = "makes", word2 = "coding", return 1.
+    */
+    public static int shortestDistance(String[] str,String word1,String word2)
+    {
+        int m=-1;
+        int n=-1;
+        int min=Integer.MAX_VALUE;
+
+        for (int i=0;i<str.length;i++)
+        {
+            if (word1.equals(str[i]))
+            {
+                m=i;
+                if (n!=-1)
+                {
+                    int dist = m-n;
+                    min = Math.min(min,dist);
+                }
+            }
+            else if (word2.equals(str[i]))
+            {
+                n=i;
+                if (m!=-1)
+                {
+                    min = Math.min(min,n-m);
+                }
+            }
+        }
+
+        return min;
+
     }
 }
 
